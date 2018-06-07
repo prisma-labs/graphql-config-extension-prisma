6503bdab43a6b7d4154277bc716439d808b8a154
Divyendu Singh <divyendu.z@gmail.com>
2018-06-07 15:06:16 +0200

    make directive @relation, onDelete field optional

--- src/index.ts
+++ src/index.ts
@@ -69,7 +69,7 @@ export function getCustomDirectives(version?: string) {
   return [
     'enum DeleteValues { CASCADE SET_NULL }',
     'directive @default(value: Any!) on FIELD_DEFINITION | SCALAR',
-    'directive @relation(name: String!, onDelete: DeleteValues!) on FIELD_DEFINITION | SCALAR',
+    'directive @relation(name: String!, onDelete: DeleteValues) on FIELD_DEFINITION | SCALAR',
     'directive @unique on FIELD_DEFINITION | SCALAR',
     'directive @pgRelation(column: String!) on FIELD_DEFINITION | SCALAR',
     'directive @pgRelationTable(table: String!, relationColumn: String!, targetColumn: String!) on FIELD_DEFINITION | SCALAR',
