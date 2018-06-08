49902fe15c6a4f6f0f577edcee1b8846f9690ed7
Divyendu Singh <divyendu.z@gmail.com>
2018-06-08 10:17:03 +0200

    tests: use snapshot testing

--- src/example/test.ts
+++ src/example/test.ts
@@ -18,28 +18,5 @@ test('patches endpoints and directives (to all projects if one of them has prism
     }
   } as any
   const newConfig = await patchConfig(config, `${__dirname}`);
-  expect(newConfig).toEqual({
-    config: {
-      projects: {
-        app: {
-          extensions: {
-            customDirectives: getCustomDirectives()
-          }
-        },
-        database: {
-          extensions: {
-            prisma: 'prisma.yml',
-            graphcool: 'graphcool.yml',
-            endpoints: {
-              dev: {
-                headers: undefined,
-                url: "https://eu1.prisma.sh/public-asd/n/dev"
-              }
-            },
-            customDirectives: getCustomDirectives()
-          },
-        },
-      },
-    }
-  });
+  expect(newConfig).toMatchSnapshot();
 });
